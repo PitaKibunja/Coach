f59c7b7179501f134686b947943d942476bd5049
DapinTechnologies <70751446+DapinTechnologies@users.noreply.github.com>
2021-07-09 15:56:02 +0300

    CoachItem:

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>
+        <h3>{{ fullName }}</h3>
+        <h4>${{ rate }}/hour</h4>
+        <div>
+           <span v-for="area in areas" :key="area">
+               {{ area }}
+           </span> 
+        </div>
+        <div class="actions">
+            
+        </div>
+    </li>
+</template>
