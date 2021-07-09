6878088edc5211a9f03d05c1eb5c83480fd15064
DapinTechnologies <70751446+DapinTechnologies@users.noreply.github.com>
2021-07-09 13:02:50 +0300

    entering vuex

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        Filter here....
+    </section>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach </router-link>
+        </div>
+        List of coaches here...
+    </section>
+</template>
