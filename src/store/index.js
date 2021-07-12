5c9a449216c9a751ec4e448608d059a471761f3a
DapinTechnologies <70751446+DapinTechnologies@users.noreply.github.com>
2021-07-12 17:24:27 +0300

    work 247 onwards

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore } from "vuex";
 
 import coachesModule from './modules/coaches/index.js'
+import requestsModule from './modules/requests/index.js'
 
 
 
 const store = createStore({
     modules: {
-        coaches:coachesModule
+        coaches: coachesModule,
+        requests:requestsModule
     },
     state() {
         return {
